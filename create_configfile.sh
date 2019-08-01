f09407011a786288ed0b3f8c000cd52d7d096df6
lexleong <46984981+lexleong@users.noreply.github.com>
2019-08-01 12:10:22 +0930

    Update create_configfile.sh

--- create_configfile.sh
+++ create_configfile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 input= ${@?Error:no input given}
-ls $input | cut -f 2 -d "/" | cut -f 1 -d "_" | awk 'BEGIN{ print "samples:"}; ( $0 !~ /NEG/ ){print "- " $0 }'
+ls $input | cut -f 2 -d "/" | cut -f 1 -d "_" | awk 'BEGIN{ print "samples:"}; ( $0 !~ /NEG/ ){print "- " $0 }' > config.yaml
 echo "This script will remove Negative controls with string "NEG" within their IDs. If it is something else, manually remove the sample from the config.yaml file"  
