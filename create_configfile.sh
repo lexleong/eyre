691ad7b2c43a7d659d7c84449dcecefa5e2042f4
lexleong <46984981+lexleong@users.noreply.github.com>
2022-05-03 14:33:47 +0930

    Delete create_configfile.sh

--- create_configfile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-input= ${@?Error:no input given}
-ls input/*_R1.fastq.gz | cut -f 2 -d "/" | cut -f 1 -d "_" | awk 'BEGIN{ print "samples:"}; ( $0 !~ /NEG/ ){print "- " $0 }' > config.yaml
-echo "This script will remove Negative controls with string "NEG" within their IDs. If it is something else, manually remove the sample from the config.yaml file"  
